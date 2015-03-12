c3ee4bca03231ca3cbe98c4cec39c2c96026b1c0
Matteo Ferrando <matteo.ferrando2@gmail.com>
2015-03-12 12:39:30 -0430

    comments in acmaker

--- acmaker.cpp
+++ acmaker.cpp
@@ -19,29 +19,35 @@ ull cache[MAX_LENGTH + 1][MAX_LENGTH + 1][2];
 
 ull rec(int i, int w, int l, bool must) {
 
+    // If just saw the last acronym letter, and the last word, 
+    // and we used at least one letter in it, then it's all good
     if (l == chrs && w == 1 && !must) return 1;
-    if (l == chrs) return 0;
-    if (phrase[i] == '\0') return 0;
 
+    // If we saw the last acronym letter, but it's not a valid usage; 
+    // or if we ran out of phrase
+    if (l == chrs || phrase[i] == '\0') return 0;
+
+    // If we've been here, don't recalculate
     if (cache[i][l][must] != -1) return cache[i][l][must];
 
-    // If changing word
     if (phrase[i] == ' ') {
-        if (must) 
-            return 0;
-        else
-            return cache[i][l][must] = rec(i+1, w-1, l, true);
+        // If changing from one word of the phrase to the next one
+        // Check if we've used at least one letter in it
+        if (must) return 0;
+        else      return cache[i][l][must] = rec(i+1, w-1, l, true);
     }
 
     char chr = acronym[l] + 32;
     ull ans = 0;
 
-    if (chr == phrase[i]) {
+    if (chr == phrase[i])
+        // If we found the current acronym letter, take it
         ans = rec(i+1, w, l+1, false);
-    }
 
+    // Or don't take it, whatever
     ans += rec(i+1, w, l, must);
 
+    // Store the result, we don't want to do this again
     return cache[i][l][must] = ans;
 }
 
@@ -64,6 +70,7 @@ int main() {
             if (strcmp(input, last_case) == 0) break;
 
             acronym = strtok(input, " ");
+            chrs = strlen(acronym);
 
             memset(phrase, 0, sizeof(phrase));
             memset(cache, -1, sizeof(cache));
@@ -87,11 +94,9 @@ int main() {
                 }
             }
 
+            // Finishing up the copying
             --phrase_size;
             phrase[phrase_size] = '\0';
-
-            // Pointing to the first word
-            chrs = strlen(acronym);
 
             // First character in the phrase, number of words, first character of the acronym
             ull result = rec(0, ws, 0, true);
